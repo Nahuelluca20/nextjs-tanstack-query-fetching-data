473f01775034b2d8754a5e1efcb72b192d827252
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-22 18:07:18 -0300

    add posts types

--- app/post/post.tsx
+++ app/post/post.tsx
@@ -1,5 +1,7 @@
 "use client";
 
-export default function Post() {
-  return <div />;
+import {PostType} from "@/types/types";
+
+export default function Post({tags, title, id, reactions, body, userId}: PostType) {
+  return <div>{title}</div>;
 }
