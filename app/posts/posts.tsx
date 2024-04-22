e0ac6e29166a1a1486439d1b116d51067068fc27
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-22 18:24:31 -0300

    move post component

--- app/posts/posts.tsx
+++ app/posts/posts.tsx
@@ -3,11 +3,19 @@
 import {useQuery} from "@tanstack/react-query";
 
 import {getPosts} from "@/queries/post-queries";
+import {PostType} from "@/types/types";
+
+import Post from "../../components/post";
 
 export default function Posts() {
   const {data} = useQuery({queryKey: ["posts"], queryFn: getPosts});
 
   console.log(data);
 
-  return <div>hello </div>;
+  return (
+    <div>
+      hello
+      {data?.map((post: PostType) => <Post {...post} key={post.id} />)}
+    </div>
+  );
 }
--- queries/post-queries.ts
+++ queries/post-queries.ts
@@ -1,5 +1,7 @@
+import {PostsType} from "@/types/types";
+
 export async function getPosts() {
-  const posts = await fetch("https://dummyjson.com/posts")
+  const posts: PostsType = await fetch("https://dummyjson.com/posts")
     .then((response) => response.json())
     .then((data) => data.posts);
 
