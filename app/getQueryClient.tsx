64683d664ca88ed3dbae72a230a4e36710d37e2e
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-28 19:29:11 -0300

    Refactor getQueryClient function in getQueryClient.tsx

--- app/getQueryClient.tsx
+++ app/getQueryClient.tsx
@@ -3,15 +3,6 @@ import {QueryClient} from "@tanstack/react-query";
 import {cache} from "react";
 
 // cache() is scoped per request, so we don't leak data between requests
-const getQueryClient = cache(
-  () =>
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          gcTime: 1000 * 60 * 60 * 24, // 24 hours
-        },
-      },
-    }),
-);
+const getQueryClient = cache(() => new QueryClient());
 
 export default getQueryClient;
